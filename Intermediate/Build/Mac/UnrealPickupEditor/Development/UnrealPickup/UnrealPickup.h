2b7e5c91ec4b4fcae8a5e6813195b61405d6c41f
Lucas Cartaxo <lucascartaxo@live.com>
2015-04-22 16:56:08 -0300

    removing unnecessary files

--- Intermediate/Build/Mac/UnrealPickupEditor/Development/UnrealPickup/UnrealPickup.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_UNREALPICKUP_H__
-#define __AUTO_UNREALPICKUP_H__
-//Last Write: 4/22/2015 7:39:16 PM +00:00
-#include "/Developer/unreal/UnrealPickup/Source/UnrealPickup/UnrealPickup.h"
-#endif//__AUTO_UNREALPICKUP_H__
--- Intermediate/IOS/build/UnrealPickup.build/DebugGame-iphonesimulator/UE4CmdLineRun.build/Script-70243947913BF79C6FAA51D1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ $DEPLOYMENT_LOCATION = "YES" ]
-then
-cp -R Engine/Binaries/IOS/Payload/UE4CmdLineRun.xctest/ $DSTROOT/$LOCAL_APPS_DIR/UE4CmdLineRun.xctest
-fi
